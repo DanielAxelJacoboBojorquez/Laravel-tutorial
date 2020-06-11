a65fc765db46f1bcf73f7d7b6fdacdea7d7a025d
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-06-11 08:41:57 -0700

    final commit

--- app/Article.php
+++ app/Article.php
@@ -9,10 +9,14 @@ class Article extends Model
     protected $guarded = [];
 
     public function path(){
-        return route('article.show', $this);
+        return route('articles.show', $this);
     }
 
-    public function user(){
-        return $this->belongsTo(User::class);
+    public function author(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 }
