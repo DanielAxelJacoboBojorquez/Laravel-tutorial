f1f1bce887eb244199c4400837e63161f4769b22
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-06-12 09:51:39 -0700

    Email

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,28 @@
 @extends('layout')
 
+@section('head')
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+@endsection
+
 @section('content')
-    <h1>Hello World!</h1>
+<div id="wrapper">
+	<div id="page" class="container">
+		<h2 class="heading has-text-weigth-bold is-size-4">Contact Me</h2>
+        <form method="POST" action="/contact" class="bg-white p-6 rounded shadow-mb"  style="width: 300px">
+            @csrf
+            <div class="mb-5">
+                <label for="email" class="block text-xs uppercase font-semibold mb-1">Email Address</label>
+                <input type="text" id="email" name="email" class="border px-2 py-1 text-sm w-full">
+                @error('email')
+                    <div class="text-red-500 text-xs">{{ $message }}</div>
+                @enderror
+                <br><br>
+                <button type="submit" class="bg-blue-500 py-2 text-white rounded-full text-sm w-full">Email Me</button>
+                @if(session('message'))
+                    <p class="text-green-500 text-xs mt-2">{{ session('message') }}</p>
+                @endif
+            </div>
+        </form>
+	</div>
+</div>
 @endsection
---
+++ resources/views/emails/contact-me.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+# A heading
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione numquam soluta non quis reprehenderit repudiandae, beatae eligendi nihil, consequuntur laboriosam voluptatibus error porro magnam debitis? Sunt, dicta. Consequuntur, sit adipisci!
+
+- A list
+- goes
+- here
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio repudiandae in officia. Alias adipisci molestiae eligendi aperiam maxime hic vitae consectetur soluta nam? Quis accusamus odio necessitatibus veniam aspernatur blanditiis.
+
+@component('mail::button', ['url' => 'https://laracasts.com'])
+Visit Laracast
+@endcomponent
+
+@endcomponent
